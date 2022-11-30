db5ff900126b5119c7b15e8b3c47d0aac88aaa3e
Jorge Luis <Desarrollador@tutanota.com>
2022-11-30 11:10:50 -0500

    Update Practica.py

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -44,34 +44,34 @@ print('-----------2--------------')
 
 
 print('-----------3--------------')
-# def ListaDeListas(lista):
-#     '''
-#     Esta función recibe una lista, que puede contener elementos que a su vez sean listas y
-#     devuelve esos elementos por separado en una lista única. 
-#     En caso de que el parámetro no sea de tipo lista, debe retornar nulo.
-#     Recibe un argumento:
-#         lista: La lista que puede contener otras listas y se convierte a una 
-#         lista de elementos únicos o no iterables.
-#     Ej:
-#         ListaDeListas([1,2,['a','b'],[10]]) debe retornar [1,2,'a','b',10]
-#         ListaDeListas(108) debe retornar el valor nulo.
-#         ListaDeListas([[1,2,[3]],[4]]) debe retornar [1,2,3,4]
-#     '''
-#     # Tu código aca:
+def ListaDeListas(lista):
+    '''
+    Esta función recibe una lista, que puede contener elementos que a su vez sean listas y
+    devuelve esos elementos por separado en una lista única. 
+    En caso de que el parámetro no sea de tipo lista, debe retornar nulo.
+    Recibe un argumento:
+        lista: La lista que puede contener otras listas y se convierte a una 
+        lista de elementos únicos o no iterables.
+    Ej:
+        ListaDeListas([1,2,['a','b'],[10]]) debe retornar [1,2,'a','b',10]
+        ListaDeListas(108) debe retornar el valor nulo.
+        ListaDeListas([[1,2,[3]],[4]]) debe retornar [1,2,3,4]
+    '''
+    # Tu código aca:
 
-#     if type(lista) != list:
-#       return None
+    if type(lista) != list:
+      return None
 
-#     Result = []
-#     for e in lista:
-#       if type(e)==list:
-#         Result.extend(ListaDeListas(e))
-#       else:
-#         Result.append(e)
-#     return Result
+    Result = []
+    for e in lista:
+      if type(e)==list:
+        Result.extend(ListaDeListas(e))
+      else:
+        Result.append(e)
+    return Result
             
-# result = ListaDeListas([[1,2,[3]],[4],6,['A', 'B','X']])
-# print(result)
+result = ListaDeListas([[1,2,[3]],[4],6,['A', 'B','X']])
+print(result)
 
 
 # New_Vec = []
@@ -154,100 +154,100 @@ print('-----------5--------------')
 
 
 print('-----------6--------------')
-def ListaRepetidos(lista):
-  '''
-  Esta función recibe como parámetro una lista y devuelve una lista de tuplas donde cada 
-  tupla contiene un valor de la lista original y las veces que se repite. Los valores 
-  de la lista original no deben estar repetidos. 
-  Debe respetarse el orden original en el que aparecen los elementos.
-  En caso de que el parámetro no sea de tipo lista debe retornar nulo.
-  Recibe un argumento:
-      lista: Será la lista que se va a evaluar.
-  Ej:
-      ListaRepetidos([]) debe retornar []
-      ListaRepetidos(['hola', 'mundo', 'hola', 13, 14]), debe retornar [('hola',2),('mundo',1),(13,1),(14,1)]
-      ListaRepetidos([1,2,2,4]) debe retornar [(1,1),(2,2),(4,1)]
-  '''
-  #Tu código aca:
-  counts = {}
-  for x in lista:
-    counts[x] = counts.get(x,0) + 1
-  return [*counts.items()]
+# def ListaRepetidos(lista):
+#   '''
+#   Esta función recibe como parámetro una lista y devuelve una lista de tuplas donde cada 
+#   tupla contiene un valor de la lista original y las veces que se repite. Los valores 
+#   de la lista original no deben estar repetidos. 
+#   Debe respetarse el orden original en el que aparecen los elementos.
+#   En caso de que el parámetro no sea de tipo lista debe retornar nulo.
+#   Recibe un argumento:
+#       lista: Será la lista que se va a evaluar.
+#   Ej:
+#       ListaRepetidos([]) debe retornar []
+#       ListaRepetidos(['hola', 'mundo', 'hola', 13, 14]), debe retornar [('hola',2),('mundo',1),(13,1),(14,1)]
+#       ListaRepetidos([1,2,2,4]) debe retornar [(1,1),(2,2),(4,1)]
+#   '''
+#   #Tu código aca:
+#   counts = {}
+#   for x in lista:
+#     counts[x] = counts.get(x,0) + 1
+#   return [*counts.items()]
 
-result = ListaRepetidos([1,2,2,4,4,'a', 'a', 'a'])
-print(result)
+# result = ListaRepetidos([1,2,2,4,4,'a', 'a', 'a'])
+# print(result)
 
 
 
 print('-----------7--------------')
-def ClaseVehiculo(tipo, color):
-    '''
-    Esta función devuelve un objeto instanciado de la clase Vehiculo, 
-    la cual debe tener los siguientes atributos:
-        Tipo:       Un valor dentro de los valores posibles: ['auto','camioneta','moto']
-        Color:      Un valor de tipo de dato string.
-        Velocidad:  Un valor de tipo de dato float, que debe inicializarse en cero.
-    y debe tener el siguiente método:
-        Acelerar(): Este método recibe un parámetro con el valor que debe incrementar a la
-                    propiedad Velocidad y luego retornarla.
-                    Si la propiedad Velocidad cobra un valor menor a cero, debe quedar en cero.
-                    Si la propiedad Velocidad cobra un valor mayor a cien, debe quedar en cien.
-    Recibe dos argumento:
-        tipo: Dato que se asignará al atributo Tipo del objeto de la clase Vehiculo
-        color: Dato que se asignará al atributo Color del objeto de la clase Vehiculo
-    Ej:
-        a = ClaseVehículo('auto','gris')
-        a.Acelerar(10) -> debe devolver 10
-        a.Acelerar(15) -> debe devolver 25
-        a.Acelerar(-10) -> debe devolver 15
-    '''
-    #Tu código aca:
-    class vehiculo(object):
-        def __init__(self, tipo, color):
-            self.tipo = tipo
-            self.color = color
-            self.velocidad = 0
-
-        def Acelerar(self, vel):
-            if vel < 0:
-                return 0
-            self.velocidad += vel
-            return self.velocidad
-    return vehiculo(tipo, color)
-
-a = ClaseVehiculo('auto','gris')
-b = a.Acelerar(10)
-print(b)
-
-
-# print('-----------8--------------')
-# def OrdenarDiccionario(diccionario_par, clave, descendente=True):
+# def ClaseVehiculo(tipo, color):
 #     '''
-#     Esta función recibe como parámetro un diccionario, cuyas listas de valores tienen el mismo
-#     tamaño y sus elementos enésimos están asociados. Y otros dos parámetros que indican
-#     la clave por la cual debe ordenarse y si es descendente o ascendente.
-#     La función debe devolver el diccionario ordenado, teniendo en cuenta de no perder la
-#     relación entre los elementos enésimos.
-#     Recibe tres argumentos:
-#         diccionario:    Diccionario a ordenar.
-#         clave:          Clave del diccionario recibido, por la cual ordenar.
-#         descendente:    Un valor booleano, que al ser verdadero indica ordenamiento ascendente y 
-#                         descendente si es falso. 
-#                         Debe tratarse de un parámetro por defecto en True.
-#     Si el parámetro diccionario no es un tipo de dato diccionario ó el parámetro clave no 
-#     se encuentra dentro de las claves del diccionario, debe devolver nulo.
+#     Esta función devuelve un objeto instanciado de la clase Vehiculo, 
+#     la cual debe tener los siguientes atributos:
+#         Tipo:       Un valor dentro de los valores posibles: ['auto','camioneta','moto']
+#         Color:      Un valor de tipo de dato string.
+#         Velocidad:  Un valor de tipo de dato float, que debe inicializarse en cero.
+#     y debe tener el siguiente método:
+#         Acelerar(): Este método recibe un parámetro con el valor que debe incrementar a la
+#                     propiedad Velocidad y luego retornarla.
+#                     Si la propiedad Velocidad cobra un valor menor a cero, debe quedar en cero.
+#                     Si la propiedad Velocidad cobra un valor mayor a cien, debe quedar en cien.
+#     Recibe dos argumento:
+#         tipo: Dato que se asignará al atributo Tipo del objeto de la clase Vehiculo
+#         color: Dato que se asignará al atributo Color del objeto de la clase Vehiculo
 #     Ej:
-#         dicc = {'clave1':['c','a','b'],
-#                 'clave2':['casa','auto','barco'],
-#                 'clave3':[1,2,3]}
-#         OrdenarDiccionario(dicc, 'clave1')          debe retornar {'clave1':['a','b','c'],
-#                                                                 'clave2':['auto','barco','casa'],
-#                                                                 'clave3':[2,3,1]}
-#         OrdenarDiccionario(dicc, 'clave3', False)   debe retornar {'clave1':['b','a','c'],
-#                                                                 'clave2':['barco','auto','casa'],
-#                                                                 'clave3':[3,2,1]}
+#         a = ClaseVehículo('auto','gris')
+#         a.Acelerar(10) -> debe devolver 10
+#         a.Acelerar(15) -> debe devolver 25
+#         a.Acelerar(-10) -> debe devolver 15
 #     '''
 #     #Tu código aca:
+#     class vehiculo(object):
+#         def __init__(self, tipo, color):
+#             self.tipo = tipo
+#             self.color = color
+#             self.velocidad = 0
+
+#         def Acelerar(self, vel):
+#             if vel < 0:
+#                 return 0
+#             self.velocidad += vel
+#             return self.velocidad
+#     return vehiculo(tipo, color)
+
+# a = ClaseVehiculo('auto','gris')
+# b = a.Acelerar(10)
+# print(b)
+
+
+print('-----------8--------------')
+def OrdenarDiccionario(diccionario_par, clave, descendente=True):
+    '''
+    Esta función recibe como parámetro un diccionario, cuyas listas de valores tienen el mismo
+    tamaño y sus elementos enésimos están asociados. Y otros dos parámetros que indican
+    la clave por la cual debe ordenarse y si es descendente o ascendente.
+    La función debe devolver el diccionario ordenado, teniendo en cuenta de no perder la
+    relación entre los elementos enésimos.
+    Recibe tres argumentos:
+        diccionario:    Diccionario a ordenar.
+        clave:          Clave del diccionario recibido, por la cual ordenar.
+        descendente:    Un valor booleano, que al ser verdadero indica ordenamiento ascendente y 
+                        descendente si es falso. 
+                        Debe tratarse de un parámetro por defecto en True.
+    Si el parámetro diccionario no es un tipo de dato diccionario ó el parámetro clave no 
+    se encuentra dentro de las claves del diccionario, debe devolver nulo.
+    Ej:
+        dicc = {'clave1':['c','a','b'],
+                'clave2':['casa','auto','barco'],
+                'clave3':[1,2,3]}
+        OrdenarDiccionario(dicc, 'clave1')          debe retornar {'clave1':['a','b','c'],
+                                                                'clave2':['auto','barco','casa'],
+                                                                'clave3':[2,3,1]}
+        OrdenarDiccionario(dicc, 'clave3', False)   debe retornar {'clave1':['b','a','c'],
+                                                                'clave2':['barco','auto','casa'],
+                                                                'clave3':[3,2,1]}
+    '''
+    #Tu código aca:
   
 
 
